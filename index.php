be092fd3e6899e5ef4a2a231a63a249da5a1fa85
mvl22 <martin@lucas-smith.co.uk>
2026-03-05 22:38:15 +0000

    Start shift to Composer

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require_once ('./.config.php');
 
 # Load and run the elections system, passing in the settings
-require_once ('./elections.php');
+require_once ('./vendor/autoload.php');
 new elections ($settings);
 
 ?>
